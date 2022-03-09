07406cd101128a319730a9cbb7f6bc6d4b876288
Zakaria Ihirri <zihirri@e1r8p9.1337.ma>
2022-03-09 19:21:28 +0100

    sucks

--- push_swap_utils/algo_100.c
+++ push_swap_utils/algo_100.c
@@ -6,11 +6,56 @@
 /*   By: zihirri <zihirri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/08 20:58:56 by zihirri           #+#    #+#             */
-/*   Updated: 2022/03/08 23:13:57 by zihirri          ###   ########.fr       */
+/*   Updated: 2022/03/09 18:24:19 by zihirri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
+
+void salam(t_list **stack1, t_list **stack2, int fst, int scnd_ind)
+{
+	int scnd;
+	int size;
+	t_list *tmp;
+
+	size = ft_lstsize(*stack1);
+	scnd = 100 - scnd_ind;
+	tmp = (*stack1);
+	while (tmp)
+	{
+		printf("fst = %d \n", fst);
+		printf("scnd = %d \n", scnd);
+		if (fst > scnd)
+		{
+			while (scnd > 0)
+			{
+				rra(stack1);
+				scnd--;
+			}
+			break ;
+			// fst++;
+		}
+		else if (fst < scnd)
+		{
+			while (fst > 0)
+			{
+				ra(stack1);
+				fst--;
+			}
+			break ;
+			// scnd--;
+		}
+		tmp = tmp->next;
+		// pb(stack1, stack2, tmp->num);
+		// tmp = tmp->next;
+	}
+		printf("stack1 = %d ", tmp->num);
+		push(stack2, tmp->num);
+		ft_pop(&tmp);
+		write(1, "pb\n", 3);
+		printf("%d\n", (*stack2)->num);
+
+}
 
 void    sort_100(t_list **stack1, t_list **stack2)
 {
@@ -20,7 +65,6 @@ void    sort_100(t_list **stack1, t_list **stack2)
     t_list *temp;
     
     index = 0;
-    
     temp = *stack1;
     while (temp)
     {
@@ -39,5 +83,7 @@ void    sort_100(t_list **stack1, t_list **stack2)
         temp = temp->next;
         index++;
     }
-    printf("%d  %d", fst, scnd);
+    printf("%d  %d\n", fst, scnd);
+    salam(stack1, stack2, fst, scnd);
+	 //printf("%d\n", index );
 }
